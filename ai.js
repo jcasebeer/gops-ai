9186249ea475ec8015174dc6f0ff21dc1b39ee54
Jack <james.casebeer@gmail.com>
2017-04-22 21:17:35 -0400

    i don't rememer

--- ai.js
+++ ai.js
@@ -1,24 +1,25 @@
-function choose_card(player_cards,ai_cards,score_cards, round_number, player_score, ai_score)
+function choose_card(player_cards,ai_cards,score_cards, round_number, player_score, ai_score,min_win)
 {
 	//There should be a smaller (~10% chance) of just going with Rando Cardrissian)
-	if(Math.random() * 100 < 10.0 || !game_winnable(player_score, ai_score, score_cards, round_number)){
+	if(Math.random() * 100 < 10.0 || game_isover(player_score, ai_score, min_win)){
 		return choose_random(player_cards, ai_cards, score_cards, round_number);
 	}
 
+	return choose_random(player_cards,ai_cards,score_cards,round_number);
 	//Down here, we'll have our more robust card-choosing algorithms based on remaining
 	//number of cards.
 	//We can hardcode a bit more as the number of cards dwindles.
 	//Before that, we can have a recursive card-chooser that goes to a limited depth
 	//before calling it a day and returning the card it has selected.
 	
-	if(round_number > 6) return choose_recurse(player_cards, ai_cards, score_cards, round_number, 0, 0);
-	else return choose_direct(player_cards, ai_cards, score_cards, round_number);
+	/*if(round_number > 6) return choose_recurse(player_cards, ai_cards, score_cards, round_number, 0, 0);
+	else return choose_direct(player_cards, ai_cards, score_cards, round_number);*/
 	
 }
 
-function game_winnable(player_score, ai_score, score_cards, round_number)
+function game_winnable(player_score, ai_score, min_win)
 {
-	var total_points = ai_score + player_score;
+	/*var total_points = ai_score + player_score;
 	var previous_cards = 0;
 	var remaining_cards = 0;
 	for(i = 0; i < 13; i++){
@@ -28,17 +29,28 @@ function game_winnable(player_score, ai_score, score_cards, round_number)
 	var points_in_play = 91 - (previous_cards - total_points);
 	if(points_in_play - ai_score > remaining_cards) return false;
 	if(points_in_play - player_score > remaining_cards) return false;
-	return true;
+	return true;*/
+	return !(player_score >= min_win);
+}
+
+
+function game_isover(player_score, ai_score, min_win)
+{
+	return (ai_score >= min_win || player_score >= min_win);
 }
 
 function choose_random(player_cards, ai_cards, score_cards, round_number){
-	var	i = Math.floor(Math.random()*13);
+	var card = Math.floor(Math.random()*13);
+	while(ai_cards[card]===0)
+		card = Math.floor(Math.random()*13);
+	return card;
+	/*var	i = Math.floor(Math.random()*13);
 	var d = 0;
 	console.log("Random Card Selected: ")
 	console.log(i);
 	while(ai_cards[i+d] === 0 && ai_cards[i-d] === 0 && d < 13){d++;}
 	if(i+d < 13 && ai_cards[i+d] === 1) return i+d;
-	if(i - d > -1 && ai_cards[i-d] === 1) return i-d;
+	if(i - d > -1 && ai_cards[i-d] === 1) return i-d;*/
 }
 
 function choose_recurse(player_cards, ai_cards, score_cards, round_number, selected, depth){
